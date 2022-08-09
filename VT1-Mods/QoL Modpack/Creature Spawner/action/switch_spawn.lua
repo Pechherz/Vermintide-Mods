6d8ad2334869bee7867af047a4b84814a74962ad
Pechherz <29700891+Pechherz@users.noreply.github.com>
2022-08-09 22:49:42 +0200

    removed previous creature spawner

--- VT1-Mods/QoL Modpack/Creature Spawner/action/switch_spawn.lua
+++
@@ -1,6 +0,0 @@
-if Managers.player.is_server then
-	local conflict_director = Managers.state.conflict
-
-	conflict_director:debug_spawn_switch_breed(0)
-	EchoConsole("Switch: " .. conflict_director._debug_breed)
-end
