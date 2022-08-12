65dca5d5693da62f4f58100858da819cda514545
Pechherz <ivan.gebel@googlemail.com>
2022-08-12 19:23:14 +0200

    relocated teleport function to main file, renamed exec file to teleport_to, mod settings now properly deactivate teleport function when set by the user

--- VT1-Mods/QoL Modpack/PlayerTeleporter/action/player_teleporter/teleport.lua
+++
@@ -1,6 +0,0 @@
-local local_player = Managers.player:local_player()
-
-local locomotion_extension = ScriptUnit.extension(local_player.player_unit, "locomotion_system")
-local conflict_director = Managers.state.conflict
-local position, distance, normal, actor = conflict_director:player_aim_raycast(conflict_director._world, false, "filter_ray_projectile")
-locomotion_extension.teleport_to(locomotion_extension, position)
