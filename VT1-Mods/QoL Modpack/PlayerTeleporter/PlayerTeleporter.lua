f5db90c5414ce30779fbda19e2c7cba4af55e415
Pechherz <29700891+Pechherz@users.noreply.github.com>
2022-08-17 02:21:50 +0200

    changed to raycast filter, that ignores enemies collision box

--- VT1-Mods/QoL Modpack/PlayerTeleporter/PlayerTeleporter.lua
+++ VT1-Mods/QoL Modpack/PlayerTeleporter/PlayerTeleporter.lua
@@ -47,7 +47,7 @@ PlayerTeleporter.teleport_to = function(self)
         local local_player = Managers.player:local_player()
         local locomotion_extension = ScriptUnit.extension(local_player.player_unit, "locomotion_system")
         local conflict_director = Managers.state.conflict
-        local position, distance, normal, actor = conflict_director:player_aim_raycast(conflict_director._world, false, "filter_ray_projectile")
+        local position, distance, normal, actor = conflict_director:player_aim_raycast(conflict_director._world, false, "filter_ray_horde_spawn")
 
         -- EchoConsole("position: " .. tostring(position))
         -- EchoConsole("distance: " .. tostring(distance))
