6eee376f8438956de0da3a34db9b131963507331
Pechherz <29700891+Pechherz@users.noreply.github.com>
2022-08-29 11:43:49 +0200

    added chat command to add upgrade tokens to the inventory

--- VT1-Mods/QoL Modpack/givetokens/givetokens.lua
+++ VT1-Mods/QoL Modpack/givetokens/givetokens.lua
@@ -1,20 +1,20 @@
 local token_type_list = {
     {
-        value = "gold_tokens",
-        text = "Orange Tokens"
-    },
-    {
-        value = "silver_tokens",
-        text = "Blue Tokens"
+        value = "iron_tokens",
+        text = "White Tokens"
     },
     {
         value = "bronze_tokens",
         text = "Green Tokens"
     },
     {
-        value = "iron_tokens",
-        text = "White Tokens"
-    }
+        value = "silver_tokens",
+        text = "Blue Tokens"
+    },
+    {
+        value = "gold_tokens",
+        text = "Orange Tokens"
+    },
 }
 
 local args = { ... }
@@ -26,28 +26,38 @@ for value in string.gmatch(args[1], "%S+") do
 end
 
 local get_help = function()
-    EchoConsole("\n")
-    EchoConsole("You need to pass 2 arguments.")
+    EchoConsole("\n-START-OF-HELP-MESSAGE-")
+
+    if expected_number_of_parameters == 1 then
+        EchoConsole("You need to pass 1 argument.")
+    else
+        EchoConsole("You need to pass " .. expected_number_of_parameters .. " arguments.")
+    end
+
     EchoConsole("/givetokens <token type> <non-zero number>")
+    EchoConsole("\n")
 
     EchoConsole("There are only 4 available tokens you can award yourself with.")
-    EchoConsole("1. iron_tokens (white tokens)")
-    EchoConsole("2. bronze_tokens (green tokens)")
-    EchoConsole("3. silver_tokens (blue tokens)")
-    EchoConsole("4. gold_tokens (orange/red tokens)")
-    EchoConsole("\n")
+
+    for index, token in ipairs(token_type_list) do
+        EchoConsole(index .. ". " .. token.value .. " (" .. token.text .. ")")
+    end
+
+    EchoConsole("\nThe following example would add 30 white tokens to your inventory:\n /givetokens iron_tokens 30")
+
+    EchoConsole("-END-OF-HELP-MESSAGE-\n")
 end
 
 if params[1] == "help" then
     get_help()
 elseif #params == expected_number_of_parameters then
     local token_type = params[1]
-    local number_of_tokens = params[2]
+    local number_of_tokens = tonumber(params[2])
 
     for index, token in ipairs(token_type_list) do
         if token_type == token.value then
             if number_of_tokens < 0 then
-                EchoConsole("No Tokens were added to your inventory.")
+                EchoConsole("No tokens were added to your inventory.")
             elseif number_of_tokens == 1 then
                 BackendUtils.add_tokens(number_of_tokens, token_type)
                 Managers.backend:commit()
@@ -59,8 +69,10 @@ elseif #params == expected_number_of_parameters then
 
                 EchoConsole(number_of_tokens .. "x " .. token.text .. " were successfully added to your inventory.")
             end
+            return
         end
     end
+    EchoConsole("The token '" .. token_type .. "' doesn't exist.")
 else
-    EchoConsole("Refer to the help page. /givetokens help")
+    EchoConsole("Refer to the following help command for more information: \n/givetokens help")
 end
